b9989de79ae3621a67d81c1d0921390f61bdceab
gmlunesa <gmlunesa@gmail.com>
2022-11-08 23:06:22 +0800

    Added some UI tweaks 🛠

---
+++ FrenDex/Converters/StringToBulletListConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenDex.Converters
+{
+    internal class StringToBulletListConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if(value == null)
+                return null;
+
+            if (parameter is null)
+                parameter = "-";
+
+            string str = value as string;
+            str = $"{parameter} {str}";
+            return str.Replace("\n", $"\n{parameter} ");
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
